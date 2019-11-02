a017371b180896d9965a375359fb98370cf61614
Shermam Miranda <shermam.miranda@hotmail.com>
2019-11-02 12:42:43 -0300

    Fix hot realoading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, World! My friend </h1>
+        <h1> Hello, World! My Friend!!</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
